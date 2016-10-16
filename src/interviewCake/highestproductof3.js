21d81f9cc2cb607a9c7895eed0596d656aa70624
Lalitha-Iyer <iyer.lalitha1@gmail.com>
2016-10-15 23:09:09 -0700

    handle negatives

--- src/interviewCake/highestproductof3.js
+++ src/interviewCake/highestproductof3.js
@@ -8,14 +8,16 @@ function myFunction(a) {
 		high3 = a[0] * a[1] * a[2];	   
 	   
 	    min = Math.min(a[0],a[1],a[2]);
-	    high2 = high3/ min;
+	    high2 = high3/ min;        
+        low2 = high3/max;
 
 		for(i=3;i<a.length;i++)
 		{
-		    high3 = Math.max(high2 * a[i] , high3, max*min*a[i]);
-			high2= Math.max(max*a[i], high2, min*a[i]);
+		    high3 = Math.max(high2 * a[i] , high3, max*min*a[i], low2 * a[i]);
+			high2= Math.max(max*a[i], high2);
 			max = Math.max(a[i],max);
 			min= Math.min(a[i],min);
+            low2 = Math.min(low2, min*a[i]);
 		}
 	}
  	 return high3;
@@ -24,4 +26,6 @@ function myFunction(a) {
 
 // run your function through some test cases here
 // remember: debugging is half the battle!
+console.log(myFunction([ -10,-2,1,-10,-10]));
+console.log(myFunction([1, 10, -5, 1, -100]));
 console.log(myFunction([1, 10, -5, 1, -100]));
